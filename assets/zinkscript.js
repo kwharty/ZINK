7f71869e8589d1fb700d4bcf68ccac3a1066fb71
Norton Nice <nortonnice@gmail.com>
2018-10-01 20:27:49 -0700

    renamed to indesx, changed some css and js

--- assets/zinkscript.js
+++ assets/zinkscript.js
@@ -1,11 +1,32 @@
 $(document).ready(function() {
-    //alert("Linked!")//
-    ;
+    //alert("Linked!")
+    
+var teamList = [
 
+ {
+    name: "Arizona Cardinals",
+    abbr: "ari",
+},
 
+{
+    name: "Carolina Panthers",
+    abbr: "car",
+}];
 
+for (var i = 0; i < teamList.length; i++) {
+    //<a class="dropdown-item" href="#">Action</a>  what i'm making 
+    var newTeamLink = $("<a>");
+    newTeamLink.attr("data-name", teamList[i].name);
+    newTeamLink.attr("data-abr", teamList[i].abbr);
+    newTeamLink.addClass("dropLink");
+    console.log(newTeamLink[0]);
 
+    newTeamLink.text(teamList[i].name);
+    $(".dropdown-item").append(newTeamLink) ;
 
+}
+
+//console.log(teamList[1].name);
 
 
 
